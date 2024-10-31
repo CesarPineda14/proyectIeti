b26339d2452aa2f6257813d2328ff58ceea5c0f1
CesarPineda14 <cesar.pineda-d@mail.escuelaing.edu.co>
2024-10-31 09:27:06 -0500

    first commit

---
+++ src/main/java/org/escuelaing/eci/repository/user/UserMongoRepository.java
@@ -0,0 +1,11 @@
+package org.escuelaing.eci.repository.user;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMongoRepository extends MongoRepository<User, String> {
+
+}
